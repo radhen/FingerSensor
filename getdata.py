c477d271652c792a60d3820b7b1337ba0e7ff053
Yang <yangautumn43@gmail.com>
2016-02-15 16:31:12 -0700

    First commit

--- getdata.py
+++ getdata.py
@@ -3,21 +3,20 @@ import re
 import serial 
 ser = serial.Serial('/dev/ttyACM0',115200)
 
-for i in range(1,100,1):
-	s = ''
-	#limb.move_to_joint_positions(angles,timeout=1.0)
-	#print(angles['left_w2'])
-	ser.write(b'm')
-	start = time.clock()
-	# read sensor strip
-	line = ser.readline()
-	stop = time.clock()
-	elasped = (stop - start)/2
-	timestamp = elasped + start
-	s = repr(timestamp)
-	line = re.sub(' +',',',line)
-	line = re.sub('\r\n','',line)
-	pre = line.split(",")
-	for _i in range(1,17,1):
-		s=s+' '+pre[_i]
-	print s
+try:
+	while 1:
+		s = ''
+		ser.write(b'm')
+		line = ser.readline()
+		#s = repr(timestamp)
+		line = re.sub(' +',',',line)
+		line = re.sub('\r\n','',line)
+		pre = line.split(",")
+		for _i in range(1,17,1):
+			s=s+' '+pre[_i]
+		print s
+
+except KeyboardInterrupt:
+	ser.close()
+except Exception:
+	ser.close()
