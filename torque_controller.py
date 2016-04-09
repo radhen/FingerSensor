6755268e84ee86df7f962a9871e5d6ca9ecc651e
radhen <radhen.17@gmail.com>
2016-04-09 11:59:05 -0600

    torquecontrollererrormeasure

--- torque_controller.py
+++ torque_controller.py
@@ -53,7 +53,10 @@ class JointSprings(object):
         self._springs = dict()
         self._damping = dict()
         self._start_angles = dict()
-        self._start_angles_leftkeys = []
+        self.left_joint_names = []
+        self.i = 1
+        self.joint_names = []
+        self.sum_sqr_error = 0
 
         # create cuff disable publisher
         cuff_ns = 'robot/limb/' + limb + '/suppress_cuff_interaction'
@@ -74,6 +77,63 @@ class JointSprings(object):
             self._damping[joint] = self._dyn.config[joint[-2:] +
                                                     '_damping_coefficient']
 
+    def move_to_neutral(self):
+        """
+        Moves the limb to neutral location.
+        """
+        self._limb.move_to_neutral()
+
+    def try_float(self,x):
+        try:
+            return float(x)
+        except ValueError:
+            return None
+
+    def _get_data(self):
+        with open('jointrecorderBax.txt', 'r') as f:
+            self.data = f.readlines()
+
+        self.joint_names = self.data[0].rstrip().split(',')
+        #parse joint names for the left and right limbs
+        for name in self.joint_names:
+            if 'left' == name[:-3]:
+                self.left_joint_names.append(name)
+
+    def _get_cmd_positions(self):
+        #convt str to float
+        self.i += 1
+        self._start_angles_1 = [self.try_float(x) for x in self.data[self.i].rstrip().split(',')] #str to float
+        cmd = dict(zip(self.joint_names, self._start_angles_1)) #formatted dict({str:float})
+        #selecting left arm values
+        left_arm_cmd = [cmd[jnts] for jnts in self.left_joint_names]
+        self._start_angles_1 = dict(zip(self.left_joint_names, left_arm_cmd)) #formatted dict({str:float})
+        return self._start_angles_1
+
+    def attach_springs(self):
+        self._start_angles = self._get_cmd_positions()
+        control_rate = rospy.Rate(self._rate)
+
+        # for safety purposes, set the control rate command timeout.
+        # if the specified number of command cycles are missed, the robot
+        # will timeout and disable
+        self._limb.set_command_timeout((1.0 / self._rate) * self._missed_cmds)
+
+        error = [self._limb.joint_angles()[key] - self._start_angles[key] for key in self._limb.joint_angles().keys()]
+        self.sum_sqr_error = sum([error[i]**2 for i in range(len(error))])
+        print ('-------new set of joint position---------')
+        print (self.sum_sqr_error)
+        tolerance = 0.020
+        # loop at specified rate commanding new joint torques
+        while self.sum_sqr_error>tolerance and not rospy.is_shutdown():
+
+            if not self._rs.state().enabled:
+                    rospy.logerr("Joint torque example failed to meet "
+                            "specified control rate timeout.")
+                    break
+            self._update_forces()
+            control_rate.sleep()
+
+
     def _update_forces(self):
         """
         Calculates the current angular difference between the start position
@@ -82,93 +142,30 @@ class JointSprings(object):
         """
         # get latest spring constants
         self._update_parameters()
-
         # disable cuff interaction
         self._pub_cuff_disable.publish()
-
         # create our command dict
         cmd = dict()
         # record current angles/velocities
         cur_pos = self._limb.joint_angles()
         cur_vel = self._limb.joint_velocities()
+
+
+        error = [self._limb.joint_angles()[key] - self._start_angles[key] for key in self._limb.joint_angles().keys()]
+        self.sum_sqr_error = sum([error[i]**2 for i in range(len(error))])
+        #print (self.sum_sqr_error)
+
         # calculate current forces
-        for joint in self._start_angles_1.keys():
+        for joint in self._start_angles.keys():
             # spring portion
-            cmd[joint] = self._springs[joint] * (self._start_angles_1[joint] -
+            cmd[joint] = self._springs[joint] * (self._start_angles[joint] -
                                                    cur_pos[joint])
             # damping portion
             cmd[joint] -= self._damping[joint] * cur_vel[joint]
         # command new joint torques
         self._limb.set_joint_torques(cmd)
-
-    def move_to_neutral(self):
-        """
-        Moves the limb to neutral location.
-        """
-        self._limb.move_to_neutral()
-
-    def attach_springs(self):
-        """
-        Switches to joint torque mode and attached joint springs to current
-        joint positions.
-        """
-        # record initial joint angles
-        with open('jointrecorderBax.txt', 'r') as f:
-            self._start_angles = f.readlines()
-
-        joint_names = self._start_angles[0].rstrip().split(',')
-        #parse joint names for the left and right limbs
-        for name in joint_names:
-            if 'left' == name[:-3]:
-                self._start_angles_leftkeys.append(name)
-
-        def try_float(x):
-            try:
-                return float(x)
-            except ValueError:
-                return None
-
-        #for values in self._start_angles[1:]:
-        #self._start_angles = self._limb.joint_angles()
-        #print (self._start_angles)
-        #self._start_angles = {'left_e0': 0.2017184736069319,
-                            #'left_e1': 0.5522330836387489,
-                            #'left_s0': -0.6070728968056525,
-                            #'left_s1': -0.36125247554701495,
-                            #'left_w0': -0.26691265709206197,
-                            #'left_w1': 1.2789564818994636,
-                            #'left_w2': -0.05714078434873166}
-
-        #self.movearm['left_s1'] = self.movearm['left_s1'] - 1
-        #print ('adding angulardisplacement to w1')
-        #print (self.movearm)
-        #self._limb.move_to_joint_positions(self.movearm)
-
-        # set control rate
-        control_rate = rospy.Rate(self._rate)
-
-        # for safety purposes, set the control rate command timeout.
-        # if the specified number of command cycles are missed, the robot
-        # will timeout and disable
-        self._limb.set_command_timeout((1.0 / self._rate) * self._missed_cmds)
-
-        # loop at specified rate commanding new joint torques
-        while not rospy.is_shutdown():
-            for values in self._start_angles[1:]:
-                values = [try_float(x) for x in values.rstrip().split(',')]
-                cleaned = zip(joint_names, values)
-                cmd = dict()
-                cmd = dict(cleaned)
-                left_cmd = [cmd[jnts] for jnts in self._start_angles_leftkeys]
-                self._start_angles_1 = dict(zip(self._start_angles_leftkeys, left_cmd))
-
-                if not self._rs.state().enabled:
-                        rospy.logerr("Joint torque example failed to meet "
-                                "specified control rate timeout.")
-                        break
-
-                self._update_forces()
-                control_rate.sleep()
+        #print ('These are the torque values')
+        #print (cmd)
 
     def clean_shutdown(self):
         """
@@ -184,15 +181,9 @@ class JointSprings(object):
 def main():
     """Joint Torque Example: Joint Springs
 
-    Moves the specified limb to a neutral location and enters
-    torque control mode, attaching virtual springs (Hooke's Law)
+    Moves the specified limb in torque control mode,
+    attaching virtual springs (Hooke's Law)
     to each joint maintaining the start position.
-
-    Run this example on the specified limb and interact by
-    grabbing, pushing, and rotating each joint to feel the torques
-    applied that represent the virtual springs attached.
-    You can adjust the spring constant and damping coefficient
-    for each joint using dynamic_reconfigure.
     """
     arg_fmt = argparse.RawDescriptionHelpFormatter
     parser = argparse.ArgumentParser(formatter_class=arg_fmt,
@@ -210,9 +201,12 @@ def main():
     js = JointSprings(args.limb, dynamic_cfg_srv)
     # register shutdown callback
     rospy.on_shutdown(js.clean_shutdown)
+    js._get_data() # get data from file
     js.move_to_neutral()
-    #print (js._limb.joint_angles())
-    js.attach_springs()
+    #while not rospy.is_shutdown() and js.i<len(js.data):
+    while len(js.data) and not rospy.is_shutdown():
+        js.attach_springs()
+
 
 
 if __name__ == "__main__":
