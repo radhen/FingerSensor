f556016e2df28ca3b65ea52d4ee96cd9c79f6190
Andy McEvoy <mcevoy.andy@gmail.com>
2016-04-15 13:39:37 -0600

    camera and table keyboard tf

--- ros_finger_sensor/src/main_experiment.cpp
+++ ros_finger_sensor/src/main_experiment.cpp
@@ -6,6 +6,12 @@
 
 #include <ros/ros.h>
 #include <rviz_visual_tools/rviz_visual_tools.h>
+
+#include <tf/transform_listener.h>
+#include <tf_conversions/tf_eigen.h>
+
+#include <Eigen/Core>
+
 #include <iostream>
 
 namespace ros_finger_sensor
@@ -18,6 +24,11 @@ private:
 
   rviz_visual_tools::RvizVisualToolsPtr visual_tools_;
 
+  tf::TransformListener tf_listener_;
+  tf::StampedTransform table_transform_;
+  Eigen::Affine3d new_pose_;
+  Eigen::Affine3d table_pose_;
+
 public:
   // Constructor
   FingerSensorTest(int test)
@@ -29,9 +40,31 @@ public:
     visual_tools_.reset(new rviz_visual_tools::RvizVisualTools("base", "visual_tools"));
     visual_tools_->deleteAllMarkers();
 
+    // get tf for table
+    ROS_INFO_STREAM_NAMED("constructor","waiting for table transform to be published...");
+    tf_listener_.waitForTransform("/base", "/table", ros::Time(0), ros::Duration(5.0));
+    tf_listener_.lookupTransform("/base", "/table", ros::Time(0), table_transform_);
+    tf::transformTFToEigen(table_transform_, table_pose_);
+    visual_tools_->publishCuboid(table_pose_, 0.6, 1.0, 0.4, rviz_visual_tools::ORANGE);
+
     while(ros::ok())
     {
-      // get tf for desk and publish cube at that location
+      tf_listener_.lookupTransform("/base", "/table", ros::Time(0), table_transform_);
+      tf::transformTFToEigen(table_transform_, new_pose_);
+
+      // test if table pose has been updated & update visualization
+      if ( !(std::abs((new_pose_.translation() - table_pose_.translation()).sum()) < 0.001 && 
+             std::abs((new_pose_.rotation() - table_pose_.rotation()).sum()) < 0.001 ) )
+        {
+          std::cout << "translation = " << new_pose_.translation() - table_pose_.translation() << std::endl;
+          std::cout << "rotation = \n" << new_pose_.rotation() - table_pose_.rotation() << std::endl;
+          ROS_DEBUG_STREAM_NAMED("constructor","update cuboid position");
+          table_pose_ = new_pose_;
+          tf::transformTFToEigen(table_transform_, new_pose_);
+          visual_tools_->deleteAllMarkers();
+          visual_tools_->publishCuboid(table_pose_, 0.6, 1.0, 0.4, rviz_visual_tools::ORANGE);
+        }
+      
     }
   }
 };
