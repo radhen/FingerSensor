2da4e9f5f37aa86dd63987f8a2c1c8ae4b89e547
Andy McEvoy <mcevoy.andy@gmail.com>
2016-04-21 13:55:29 -0600

    visualize simple motion in planning model

--- ros_finger_sensor/src/baxter_control.cpp
+++ ros_finger_sensor/src/baxter_control.cpp
@@ -8,6 +8,15 @@
 
 #include <sensor_msgs/JointState.h>
 
+/*
+  TODO:
+  1. get pose of object from perception server
+  2. generate simple path from "ready position" to object
+  3. visualize for operator
+  4. get ok from operator to execute
+  5. publish commmand to /robot/limb/(right/left)/joint_command
+*/
+
 namespace ros_finger_sensor
 {
 
@@ -17,12 +26,71 @@ private:
   ros::NodeHandle nh_;
   ros::Publisher planning_state_pub_;
   unsigned long int seq_;
+  sensor_msgs::JointState planning_msg_;
+
+  double joint_limits[15][2];
 
 public:
   //Constructor
   BaxterControlTest(int test)
   {
     ROS_DEBUG_STREAM_NAMED("constructor","starting test " << test);
+    
+    // define limits
+    joint_limits[0][0] = -1.57079632679;
+    joint_limits[0][1] = 1.57079632679; // head_pan 
+    joint_limits[1][0] = -1.70167993878; 
+    joint_limits[1][1] = 1.7016799387; //right_s0
+    joint_limits[2][0] = -2.147; 
+    joint_limits[2][1] = 1.047; //right_s1
+    joint_limits[3][0] = -3.05417993878; 
+    joint_limits[3][1] = 3.05417993878; //right_e0
+    joint_limits[4][0] = -0.05; 
+    joint_limits[4][1] = 2.618; //right_e1
+    joint_limits[5][0] = -3.059; 
+    joint_limits[5][1] = 3.059; //right_w0
+    joint_limits[6][0] = -1.57079632679; 
+    joint_limits[6][1] = 2.094; //right_w1
+    joint_limits[7][0] = -3.059; 
+    joint_limits[7][1] = 3.059; //right_w2
+    joint_limits[8][0] = -1.70167993878; 
+    joint_limits[8][1] = 1.70167993878; //left_s0 
+    joint_limits[9][0] = -2.147; 
+    joint_limits[9][1] = 1.047; //left_s1 
+    joint_limits[10][0] = -3.05417993878; 
+    joint_limits[10][1] = 3.05417993878; //left_e0 
+    joint_limits[11][0] = -0.05; 
+    joint_limits[11][1] = 2.618; //left_e1 
+    joint_limits[12][0] = -3.059; 
+    joint_limits[12][1] = 3.059; //left_w0 
+    joint_limits[13][0] = -1.57079632679; 
+    joint_limits[13][1] = 2.094; //left_w1 
+    joint_limits[14][0] = -3.059; 
+    joint_limits[14][1] = 3.059; //left_w2 
+
+    for (int i = 0; i < 15; i++)
+    {
+      planning_msg_.position.push_back(0.0);
+      planning_msg_.velocity.push_back(0.0);
+      planning_msg_.effort.push_back(0.0);
+    }
+
+    // setup planning message
+    planning_msg_.name.push_back("head_pan");
+    planning_msg_.name.push_back("right_s0");
+    planning_msg_.name.push_back("right_s1");
+    planning_msg_.name.push_back("right_e0");
+    planning_msg_.name.push_back("right_e1");
+    planning_msg_.name.push_back("right_w0");
+    planning_msg_.name.push_back("right_w1");
+    planning_msg_.name.push_back("right_w2");
+    planning_msg_.name.push_back("left_s0");
+    planning_msg_.name.push_back("left_s1");
+    planning_msg_.name.push_back("left_e0");
+    planning_msg_.name.push_back("left_e1");
+    planning_msg_.name.push_back("left_w0");
+    planning_msg_.name.push_back("left_w1");
+    planning_msg_.name.push_back("left_w2");
 
     // Set up subs/pubs
     seq_ = 0;
@@ -36,45 +104,31 @@ public:
 
   void publishPlanningState()
   {
-    ROS_DEBUG_STREAM_NAMED("publishPlanningState","populating message " << seq_);
-    sensor_msgs::JointState planning_msg;
-    planning_msg.header.seq = seq_;
+    planning_msg_.header.seq = seq_;
     seq_++;
-    planning_msg.header.stamp = ros::Time::now();
+    planning_msg_.header.stamp = ros::Time::now();
 
-    planning_msg.name.push_back("head_pan");
-    planning_msg.name.push_back("right_s0");
-    planning_msg.name.push_back("right_s1");
-    planning_msg.name.push_back("right_e0");
-    planning_msg.name.push_back("right_e1");
-    planning_msg.name.push_back("right_w0");
-    planning_msg.name.push_back("right_w1");
-    planning_msg.name.push_back("right_w2");
-    planning_msg.name.push_back("left_s0");
-    planning_msg.name.push_back("left_s1");
-    planning_msg.name.push_back("left_e0");
-    planning_msg.name.push_back("left_e1");
-    planning_msg.name.push_back("left_w0");
-    planning_msg.name.push_back("left_w1");
-    planning_msg.name.push_back("left_w2");
-
-    /*
-      TODO:
-      1. get pose of object from perception server
-      2. generate simple path from "ready position" to object
-      3. visualize for operator
-      4. get ok from operator to execute
-      5. publish commmand to /robot/limb/(right/left)/joint_command
-     */
-
-    for (int i = 0; i < 15; i++)
-    {
-      planning_msg.position.push_back(0.0);
-      planning_msg.velocity.push_back(0.0);
-      planning_msg.effort.push_back(0.0);
-    }
+    // TODO: replace
+    maxMinLimits();
     
-    planning_state_pub_.publish(planning_msg);
+    planning_state_pub_.publish(planning_msg_);
+    ros::Duration(0.2).sleep();
+  }
+
+  void maxMinLimits()
+  {
+    // just a simple loop going from min to max joint limits then resetting
+    for (std::size_t i = 0; i < 15; i++)
+    {
+      double delta = (joint_limits[i][1] - joint_limits[i][0]) / 25.0;
+      ROS_DEBUG_STREAM_NAMED("maxMinLimits","delta = " << delta);
+      planning_msg_.position[i] += delta;
+      if (planning_msg_.position[i] > joint_limits[i][1])
+      {
+        planning_msg_.position[i] = joint_limits[i][0];
+      }
+      ROS_DEBUG_STREAM_NAMED("maxMinLimits","position = " << i << ", value = " << planning_msg_.position[i]);
+    }    
   }
 
 };
