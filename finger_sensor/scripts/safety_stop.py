7252ac883bc87ffacec1bafc9ce3cdab4415f7ce
Jorge Cañardo Alastuey <jorgecanardo@gmail.com>
2016-06-22 12:21:46 -0600

    ENH: 1st pass at using finger sensor as safety stops/to control arm

---
+++ finger_sensor/scripts/safety_stop.py
@@ -0,0 +1,112 @@
+#! /usr/bin/env python
+from __future__ import division, print_function, absolute_import
+
+import numpy as np
+
+import rospy
+import tf
+from std_msgs.msg import Header, Int32MultiArray
+from geometry_msgs.msg import Point, Quaternion, Pose, PoseStamped
+
+from .robot import Baxter
+
+
+class SafetyStop(object):
+    """In theory, if this node is running, no matter what happens, if
+    something gets too close to the sensor tip, the robot will stop
+    moving until the obstacle is cleared.
+
+    """
+    def __init__(self, topic='/sensor_values'):
+        self.bx = Baxter()
+        self.joint_v = {name: 0.0 for name in self.bx.limb.joint_names()}
+        self.stop = False
+        self.nh = rospy.init_node('SafetyStop')
+        self.sensor_sub = rospy.Subscriber(topic,
+                                           Int32MultiArray,
+                                           self.udpate_joint_v,
+                                           queue_size=1)
+
+    def _cartesian_v_from_sensor_values(self, values):
+        tip = values[[7, 15]]
+        if np.any(tip < 10**4):
+            self.stop = True
+        else:
+            self.stop = False
+        return [0] * 6
+
+    def update_joint_v(self, msg):
+        arr = np.array(msg.data)
+        cartesian_v = self._cartesian_v_from_sensor_values(arr)
+        self.joint_v = self.bx.compute_joint_velocities(cartesian_v)
+
+
+class ControlArmThroughHand(object):
+    def __init__(self, topic='/sensor_values'):
+        self.bx = Baxter()
+        self.joint_v = {name: 0.0 for name in self.bx.limb.joint_names()}
+        self.nh = rospy.init_node('ArmController')
+        self.sensor_sub = rospy.Subscriber(topic,
+                                           Int32MultiArray,
+                                           self.udpate_joint_v,
+                                           queue_size=1)
+        self.br = tf.TransformBroadcaster()
+        self.tl = tf.TransformListener()
+
+    def _cartesian_v_from_sensor_values(self, values):
+        log_values = np.log(values)
+        tip = log_values[[7, 15]]
+        # Match which side is which. Ideally, if the sign of the diff
+        # matches whether the gripper needs to move towards the
+        # positive or negative part of the y axis in left_gripper.
+        # That is, we need left side - right side (using left/right
+        # like in l_gripper_{l, r}_finger_tip tfs)
+        # TODO: might want to take log(values) for a better behaved controller
+        inside_diff = log_values[8:] - log_values[:7]
+        scalar_diff = sum(inside_diff) / len(inside_diff)
+
+        # Take negative for one of the sides, so that angles should
+        # match for a parallel object in the gripper
+        l_angle, _ = np.polyfit(np.arange(7), -log_values[:7], 1)
+        r_angle, _ = np.polyfit(np.arange(7), log_values[8:], 1)
+        rospy.loginfo('Angle computed from l: {}'.format(l_angle))
+        rospy.loginfo('Angle computed from r: {}'.format(r_angle))
+        avg_angle = np.arctan((l_angle + r_angle) / 2.0)
+
+        # Let's get a frame by the middle of the end effector
+        # p = Point(0, 0, -0.05)
+        p = (0, 0, -0.05)
+        # Of course, tf uses (w, x, y, z), Quaternion uses x, y, z,
+        # w. However, tf.TransformBroadcaster().sendTransform uses the
+        # tf order.
+        # q = Quaternion(q[1], q[2], q[3], q[0])
+        q = tf.transformations.quaternion_about_axis(avg_angle, (1, 0, 0))
+        time = rospy.Time.now()
+        self.br.sendTransform(p,
+                              q,
+                              time,
+                              'oriented_object_in_hand',
+                              'left_gripper')
+        p = (0, scalar_diff / 100, 0.05)
+        h = Header()
+        h.frame_id = 'oriented_object_in_hand'
+        h.stamp = time
+        p = Point(*p)
+        pose = Pose(p, Quaternion(0, 0, 0, 1))
+        new_endpose = self.tl.transformPoint("base", PoseStamped(h, pose))
+        self.bx.move_ik(new_endpose)
+
+    def update_joint_v(self, msg):
+        arr = np.array(msg.data)
+        cartesian_v = self._cartesian_v_from_sensor_values(arr)
+        self.joint_v = self.bx.compute_joint_velocities(cartesian_v)
+
+
+if __name__ == '__main__':
+    rospy.init_node('SafetyStop')
+    s = SafetyStop()
+    r = rospy.Rate(10)
+    while not rospy.is_shutdown():
+        if s.stop:
+            s.bx.limb.set_joint_velocities(s.joint_v)
+        r.sleep()
