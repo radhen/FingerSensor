68d8e313dba19f80a7598c2b57882647e57dc76c
Jorge Cañardo Alastuey <jorgecanardo@gmail.com>
2016-07-25 14:02:26 -0600

    ENH: lazy sleeps to avoid maxing CPU

--- finger_sensor/scripts/sensor.py
+++ finger_sensor/scripts/sensor.py
@@ -6,8 +6,8 @@ import rospy
 from std_msgs.msg import Int32MultiArray, MultiArrayLayout, MultiArrayDimension
 
 
-def collect_data():
-    with serial.Serial('/dev/ttyACM1', 57600, timeout=0.1) as ser:
+def collect_data(port='/dev/ttyACM0'):
+    with serial.Serial(port, 57600, timeout=0.1) as ser:
         ser.flushInput()
         # Give it some time to initialize
         data = []
@@ -18,12 +18,14 @@ def collect_data():
             rospy.sleep(1)
         print('\n'.join(filter(None, data)))
         buffer = []
+        r = rospy.Rate(100)
         while not rospy.is_shutdown():
             buffer.append(ser.read(ser.inWaiting()))
             foo = ''.join(buffer).splitlines()
             try:
                 last_full_line = foo[-2]
             except IndexError:
+                r.sleep()
                 continue
             try:
                 values = [int(i) for i in last_full_line.split()]
@@ -32,12 +34,15 @@ def collect_data():
                     yield values
             except ValueError:
                 rospy.loginfo(last_full_line)
+                r.sleep()
                 continue
             buffer = [foo[-1]]
+            r.sleep()
 
 
 def sensor_node():
     c = collect_data()
+    #c = collect_data(port='/dev/ttyACM1')
     pub = rospy.Publisher('/sensor_values', Int32MultiArray, queue_size=1)
     rospy.init_node('sensor_node')
     rate = rospy.Rate(50)
